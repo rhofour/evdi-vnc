d66427076c1b0fefacc3e712ebeb6ebe7ed45b97
Richard Alex Hofer <rhofer@alumni.cmu.edu>
2017-01-16 09:43:27 -0500

    Add initial libvncserver code.

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 all: evdi-vnc
 
-evdi-vnc: evdi-vnc.c
+evdi-vnc: evdi-vnc.o
+	$(CC) evdi-vnc.o -o evdi-vnc -lvncserver
+
+evdi-vnc.o: evdi-vnc.c
+	$(CC) -c evdi-vnc.c -o evdi-vnc.o
--- evdi-vnc.c
+++ evdi-vnc.c
@@ -1,3 +1,15 @@
+#include <rfb/rfb.h>
+
+// Bytes per pixel
+#define BPP 1
+// Screen width
+#define SCREEN_WIDTH 1280
+// Screen height
+#define SCREEN_HEIGHT 720
+
 int main(int argc, char *argv[]) {
+  rfbScreenInfoPtr screen = rfbGetScreen(&argc, argv, SCREEN_WIDTH, SCREEN_HEIGHT, 8, 3, BPP);
+  screen->frameBuffer = (char*) malloc(SCREEN_WIDTH * SCREEN_HEIGHT * BPP);
+  rfbInitServer(screen);
   return(0);
 }
